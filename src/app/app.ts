5ce981fe0df0679afd8f73619af312a68dff7d9c
chursin.a.v <chursin.a.v@cifro.tech>
2025-08-16 18:32:34 +0300

    setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,12 @@ import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+      RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    TuiRoot,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
